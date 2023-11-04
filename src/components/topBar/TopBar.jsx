74783a314d9b1388c62e8159c399e77605b3425f
Iamstefanoe <stefanoar3@gmail.com>
2023-11-04 17:23:58 +0100

    feat: status code&description fetch&display, Json

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -4,23 +4,46 @@ import { SearchIcon } from "../SearchIcon";
 
 const TopBar = ({ onSubmit }) => {
   const [url, setUrl] = useState("");
+  const [method, setMethod] = useState("");
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    onSubmit(url);
+    try {
+      const res = await fetch(
+        "https://api.myjson.online/v1/records/ab6c7e09-7254-4fd3-82d3-b1b14c373861"
+      );
+      const data = await res.json();
+      setResponse(data.data[method]);
+      onSubmit(url);
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
     <>
       <div className={styles.topBar}>
+        {error && <p>Error: {error}</p>}
+        {response && (
+          <>
+            <p>Status Code: {response.statusCode}</p>
+            <p>Description: {response.description}</p>
+          </>
+        )}
         <form className={styles.topBar__form} onSubmit={handleSubmit}>
-          <select name="methods" className={styles.topBar__form__select}>
-            <option value="methGet">GET</option>
-            <option value="methPost">POST</option>
-            <option value="methPut">PUT</option>
-            <option value="methDelete">DELETE</option>
-            <option value="methInfo">INFO</option>
-            <option value="methDumb">DUMB</option>
+          <select
+            name="methods"
+            className={styles.topBar__form__select}
+            onChange={(e) => setMethod(e.target.value)}
+          >
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="DELETE">DELETE</option>
+            <option value="INFO">INFO</option>
+            <option value="DUMB">DUMB</option>
           </select>
 
           <input
